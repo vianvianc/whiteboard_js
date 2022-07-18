37fd9a053087106298c07728b8698e4fbcfbb84a
Vian Charbonneau <102205118+vianvianc@users.noreply.github.com>
2022-07-18 10:08:53 -0600

    pseudocode

---
+++ duolicates.js
@@ -0,0 +1,16 @@
+//Remove duplicate members of an array
+
+// Create an array to store values that have been parsed over at least once in a loop; "duplicates array"
+/* Generate a loop that will go through all the elements in the array. Elements parsed through
+   only once will be stored in the duplicates array. */
+/* Loop will have conditional statement to return new array of elements only if they aren't within
+duplicates array. */
+// return unique elements array.
+// Log unique elements
+
+
+
+// Create an array to store values that have been parsed over at least once in a loop; "uniques array"
+let uniqueArr = [];
+/* Generate a loop that goes through all the elements in the array. Elements parsed through only once will be stored in the uniques array. */
+
