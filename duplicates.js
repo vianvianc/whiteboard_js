e26485a59debac1d190a84573ab77f10373df958
Vian Charbonneau <102205118+vianvianc@users.noreply.github.com>
2022-07-19 22:09:45 -0600

    pseudocode

--- duplicates.js
+++ duplicates.js
@@ -13,4 +13,13 @@ duplicates array. */
 // Create an array to store values that have been parsed over at least once in a loop; "uniques array"
 let uniqueArr = [];
 /* Generate a loop that goes through all the elements in the array. Elements parsed through only once will be stored in the uniques array. */
-
+function removeDuplicates(arr) {
+    /* Loop will have conditional statement to return new array of elements only if they aren't within uniques array. */
+    return arr.filter(function(element, index){
+        if (uniqueArr.indexOf(element) === -1) {
+            uniqueArr.push(element);
+            return element;
+        }
+    });
+}
+comsole.log(removeDuplicates(["test", "test", true, 1,1,3,-1,5,5]));
